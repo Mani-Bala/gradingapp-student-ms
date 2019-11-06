e4523bae49d6d13ff83353bd85e16492011ae7c1
Manikandan8056 <manikandanbala002@gmail.com>
2019-11-06 11:01:15 +0000

    added firebase link in cors config

--- src/main/java/com/revature/grademanagementsystemstudentms/configuration/CorsConfiguration.java
+++ src/main/java/com/revature/grademanagementsystemstudentms/configuration/CorsConfiguration.java
@@ -5,17 +5,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer
-{
-     @Override
-        public void addCorsMappings(CorsRegistry registry) {
+public class CorsConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
 
-            registry.addMapping("/**")
-                    .allowedOrigins(
-                            "http://localhost:4200")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD")
-                    .allowCredentials(true)
-            ;
-        }
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:8080", "https://gradingapp-43d72.firebaseapp.com",
+						"https://resulttracker.in")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD").allowCredentials(true);
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD").allowCredentials(true);
+	}
 }
- 
